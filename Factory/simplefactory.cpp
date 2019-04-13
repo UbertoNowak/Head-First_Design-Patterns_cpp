a3680a2023d10b10393326cebbb0337851bc9ff2
UbertoNowak <uberto89@o2.pl>
2019-04-13 20:45:05 +0200

    Simple factory pattern

--- Factory/simplefactory.cpp
+++ Factory/simplefactory.cpp
@@ -1,1 +1,77 @@
+#include <iostream>
 #include "simplefactory.h"
+
+Pizza *SimpleFactory::createPizza(PizzaType aType)
+{
+    Pizza* pizza = nullptr;
+    switch(aType)
+    {
+    case PizzaType::Cheese:
+        pizza = new CheesePizza();
+        break;
+    case PizzaType::Peperoni:
+        pizza = new PepperoniPizza();
+        break;
+    case PizzaType::Seefood:
+        pizza = new SeefoodPizza();
+        break;
+    }
+    return pizza;
+}
+
+Pizzeria::Pizzeria(SimpleFactory aFactory) : m_Factory(aFactory)
+{}
+
+Pizza* Pizzeria::orderPizza(PizzaType aType)
+{
+  Pizza* pPizza;
+  pPizza = m_Factory.createPizza(aType);
+  pPizza->preparation();
+  pPizza->baking();
+  pPizza->slicing();
+  pPizza->packing();
+  return pPizza;
+}
+
+Pizza::~Pizza()
+{}
+
+void Pizza::baking()
+{
+    std::cout<< "Baking." <<std::endl;
+}
+
+void Pizza::slicing()
+{
+    std::cout<< "Slicing." <<std::endl;
+}
+
+void Pizza::packing()
+{
+    std::cout<< "Packing." <<std::endl;
+}
+
+void CheesePizza::preparation()
+{
+    std::cout<< "Prepare cheese pizza." <<std::endl;
+}
+
+void PepperoniPizza::preparation()
+{
+    std::cout<< "Prepare pepperoni pizza." <<std::endl;
+}
+
+void SeefoodPizza::preparation()
+{
+    std::cout<< "Prepare seefood pizza." <<std::endl;
+}
+
+int main()
+{
+    Pizza* pPizza = nullptr;
+    SimpleFactory factory;
+    Pizzeria italianPizzeria(factory);
+    pPizza = italianPizzeria.orderPizza(PizzaType::Peperoni);
+    delete  pPizza;
+    return 0;
+}
--- Factory/simplefactory.h
+++ Factory/simplefactory.h
@@ -1,31 +1,56 @@
 #ifndef SIMPLEFACTORY_H
 #define SIMPLEFACTORY_H
 
-class Pizzeria
-{
-public:
-    void zamowPizze();
-};
+class Pizza;
 
+enum class PizzaType
+{
+    Cheese,
+    Peperoni,
+    Seefood
+};
 
 class SimpleFactory
 {
 public:
-    static void createPizza();
+    static Pizza* createPizza(PizzaType aType);
 };
 
 class Pizza
 {
 public:
-    void przygotowanie();
-    void pieczenie();
-    void krojenie();
-    void pakowanie();
+    virtual ~Pizza();
+    virtual void preparation() = 0;
+    void baking();
+    void slicing();
+    void packing();
 };
 
-class PizzaSerowa : public Pizza
+class CheesePizza : public Pizza
 {
+public:
+    void preparation();
+};
 
+class PepperoniPizza : public Pizza
+{
+public:
+    void preparation();
+};
+
+class SeefoodPizza : public Pizza
+{
+public:
+    void preparation();
+};
+
+class Pizzeria
+{
+public:
+    Pizzeria(SimpleFactory aFactory);
+    Pizza* orderPizza(PizzaType aType);
+private:
+    SimpleFactory m_Factory;
 };
 
 #endif // SIMPLEFACTORY_H
